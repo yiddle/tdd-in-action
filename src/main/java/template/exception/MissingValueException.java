6f22feb1574c4fd95c9dc530f6aba3e5605a546d
Yi Doe <nadew_y@ukw.de>
2020-01-26 00:37:31 +0100

    Test red: Missing value in the template #test

---
+++ src/main/java/template/exception/MissingValueException.java
@@ -0,0 +1,4 @@
+package template.exception;
+
+public class MissingValueException extends RuntimeException{
+}
--- src/test/java/template/TestTemplate.java
+++ src/test/java/template/TestTemplate.java
@@ -2,7 +2,9 @@ package template;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import template.exception.MissingValueException;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 
 class TestTemplate {
@@ -29,6 +31,15 @@ class TestTemplate {
   void unknownVariablesIgnored() throws Exception{
     template.set("doesnotexist", "alksdlak");
     assertThat(template.evaluate()).isEqualTo("1 2 3");
+  }
+
+
+  @Test
+  void testMissingValueRaisesException() {
+    Template template = new Template("${non-existing-var}");
+    assertThatThrownBy(() -> {
+      String result = template.evaluate();
+    }).isInstanceOf(MissingValueException.class);
   }
 
 }
